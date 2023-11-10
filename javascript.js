e68e2c02e76c952d349ba734041d794d3608f838
Sunetra Bhowmick <matrix_hunt@ymail.com>
2023-11-10 17:27:32 +0530

    Design finalised and README.md updated

--- javascript.js
+++ javascript.js
@@ -3,13 +3,19 @@ let adviceContainer = document.getElementById("advice")
 let id = document.getElementById("advice-id")
 let text = document.getElementById("advice-text")
 
+let loader = document.getElementById("loader")
+
 let apiUrl = "https://api.adviceslip.com/advice"
 
 window.onload = (e) => {
+    loader.style.display = "block"
     generateAdvice()
 }
 
 refresh.addEventListener("click", (e) => {
+    loader.style.display = "block"
+    id.innerHTML = "#"
+    text.innerHTML = "Loading ...."
     generateAdvice()
 })
 
@@ -19,11 +25,12 @@ function generateAdvice() {
     fetch(apiUrl)
     .then(response => response.json())
     .then(data => populateAdvice(data))
-    .catch(error => console.error('Error:', error));
+    .catch(error => console.error('Some error has occured', error));
 
     function populateAdvice(json) {
+        loader.style.display = "none"
         id.innerHTML = json.slip.id;
-        text.innerHTML = json.slip.advice
+        text.innerHTML = `"${json.slip.advice}"`;
     }
 }
 
